280e515d3781f17ef59807e912529404766b2f25
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-11-15 18:58:14 +0800

    s

--- src/main/java/net/fabricmc/example/api/command/IBaritoneChatControl.java
+++
@@ -1,7 +0,0 @@
-package net.fabricmc.example.api.command;
-
-import java.util.UUID;
-
-public interface IBaritoneChatControl {
-    String FORCE_COMMAND_PREFIX = String.format("<<%s>>", UUID.randomUUID().toString());
-}
--- src/main/java/net/fabricmc/example/api/command/ICommandSystem.java
+++
@@ -1,7 +0,0 @@
-package net.fabricmc.example.api.command;
-
-import baritone.api.command.argparser.IArgParserManager;
-
-public interface ICommandSystem {
-    IArgParserManager getParserManager();
-}
