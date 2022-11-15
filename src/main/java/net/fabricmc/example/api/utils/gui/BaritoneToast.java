53e8969fc8b8cc74a6ba4367a10ed0641d7ada8e
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-11-15 18:30:47 +0800

    Update BaritoneToast.java

--- src/main/java/net/fabricmc/example/api/utils/gui/BaritoneToast.java
+++ src/main/java/net/fabricmc/example/api/utils/gui/BaritoneToast.java
@@ -23,39 +23,35 @@ public class BaritoneToast implements IToast{
                         this.firstDrawTime = delta;
                         this.newDisplay = false;
                     }
-
         toastGui.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("textures/gui/toasts.png"));
                 GlStateManager.color(1.0F, 1.0F, 1.0F, 255.0f);
                 toastGui.drawTexturedModalRect(0, 0, 0, 32, 160, 32);
-
-        if (this.subtitle == null) {
-                        toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 12, -11534256);
-                    } else {
-                        toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 7, -11534256);
-                        toastGui.getMinecraft().fontRenderer.drawString(this.subtitle, 18, 18, -16777216);
-                    }
+                if (this.subtitle == null) {
+                    toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 12, -11534256);
+                } else {
+                    toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 7, -11534256);
+                    toastGui.getMinecraft().fontRenderer.drawString(this.subtitle, 18, 18, -16777216);
+                }
 
         return delta - this.firstDrawTime < totalShowTime ? Visibility.SHOW : Visibility.HIDE;
-            }
-
-    public void setDisplayedText(ITextComponent titleComponent, ITextComponent subtitleComponent) {
-                this.title = titleComponent.getFormattedText();
-                this.subtitle = subtitleComponent == null ? null : subtitleComponent.getFormattedText();
-                this.newDisplay = true;
-            }
-
-    public static void addOrUpdate(GuiToast toast, ITextComponent title, ITextComponent subtitle, long totalShowTime) {
-                BaritoneToast baritonetoast = toast.getToast(BaritoneToast.class, new Object());
-
-        if (baritonetoast == null) {
-                      toast.add(new BaritoneToast(title, subtitle, totalShowTime));
-                } else {
-                    baritonetoast.setDisplayedText(title, subtitle);
-            }
     }
 
-public static void addOrUpdate(ITextComponent title, ITextComponent subtitle) {
-        addOrUpdate(net.minecraft.client.Minecraft.getMinecraft().getToastGui(), title, subtitle, baritone.api.BaritoneAPI.getSettings().toastTimer.value);
+    public void setDisplayedText(ITextComponent titleComponent, ITextComponent subtitleComponent) {
+        this.title = titleComponent.getFormattedText();
+        this.subtitle = subtitleComponent == null ? null : subtitleComponent.getFormattedText();
+        this.newDisplay = true;
+    }
+
+    public static void addOrUpdate(GuiToast toast, ITextComponent title, ITextComponent subtitle, long totalShowTime) {
+        BaritoneToast baritonetoast = toast.getToast(BaritoneToast.class, new Object());
+        if (baritonetoast == null) {
+            toast.add(new BaritoneToast(title, subtitle, totalShowTime));
+        } else {
+            baritonetoast.setDisplayedText(title, subtitle);
         }
+    }
+
+    public static void addOrUpdate(ITextComponent title, ITextComponent subtitle) {
+        addOrUpdate(net.minecraft.client.Minecraft.getMinecraft().getToastGui(), title, subtitle, baritone.api.BaritoneAPI.getSettings().toastTimer.value);
     }
 }
