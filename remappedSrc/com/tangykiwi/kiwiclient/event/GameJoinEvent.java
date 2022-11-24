cb904116729fd9a078badfcb3b1af1b81e3c56c4
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-11-24 10:03:16 +0800

    s

--- remappedSrc/com/tangykiwi/kiwiclient/event/GameJoinEvent.java
+++
@@ -1,4 +0,0 @@
-package com.tangykiwi.kiwiclient.event;
-
-public class GameJoinEvent extends Event {
-}
--- remappedSrc/com/tangykiwi/kiwiclient/mixininterface/IChatHUDLine.java
+++
@@ -1,7 +0,0 @@
-package com.tangykiwi.kiwiclient.mixininterface;
-
-public interface IChatHUDLine {
-    int getId();
-
-    void setId(int id);
-}
