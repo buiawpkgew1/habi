7d129882523396d4b6ba14f7cabf65f0232de7e6
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-11-26 20:04:00 +0800

    Revert "w"
    This reverts commit 413ac3a0839497778473b08d1315f67cb29c763e.

---
+++ remappedSrc/net/fabricmc/example/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.fabricmc.example.mixin;
+
+import net.fabricmc.example.HabiMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+        HabiMod.LOGGER.info("这一行是由示例mod mixin打印的!");
+	}
+}
