28aa34acedd5a6e850df4bb6f376a1b4749405eb
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-11-26 14:16:45 +0800

    s

--- remappedSrc/net/fabricmc/example/HabiMod.java
+++ remappedSrc/net/fabricmc/example/HabiMod.java
@@ -2,11 +2,7 @@ package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.example.item.ModFoodComponents;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,11 +12,9 @@ public class HabiMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("habi");
     // 新物品的实例
-    public static Item FRUIT_APP = new Item(new Item.Settings().group(ItemGroup.FOOD)
-    .food(ModFoodComponents.FRUIT_APP));
-
+    public static Item FRUIT_APP = new Item(new Item.Settings().food(ModFoodComponents.FRUIT_APP));
+    public static Item HABI_SED = new Item(new Item.Settings().food(ModFoodComponents.HABI_SED));
 	@Override
 	public void onInitialize() {
-        Registry.register(Registry.ITEM,new Identifier("habi","frur_pudding"),FRUIT_APP);
 	}
 }
