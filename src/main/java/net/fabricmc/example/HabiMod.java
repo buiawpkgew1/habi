c0fd99558c0ab099ea6afb4ab5c3ad0cafa1a17a
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-11-27 10:47:46 +0800

    s

--- src/main/java/net/fabricmc/example/HabiMod.java
+++ src/main/java/net/fabricmc/example/HabiMod.java
@@ -2,7 +2,13 @@ package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.example.item.ModFoodComponents;
-import net.minecraft.item.Item;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +20,17 @@ public class HabiMod implements ModInitializer {
     // 新物品的实例
     public static Item FRUIT_APP = new Item(new Item.Settings().food(ModFoodComponents.FRUIT_APP));
     public static Item HABI_SED = new Item(new Item.Settings().food(ModFoodComponents.HABI_SED));
+    public static ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier("example", "test_group"))
+    .displayName(Text.literal("Example Item Group"))
+    .icon(() -> new ItemStack(Items.DIAMOND))
+    .entries((enabledFeatures, entries, operatorEnabled) -> {
+        entries.add(Items.DIAMOND);
+    })
+    .build();
 	@Override
 	public void onInitialize() {
+        Registry.register(Registries.ITEM,new Identifier("habi","app"),FRUIT_APP);
+        Registry.register(Registries.ITEM,new Identifier("habi","sed"),HABI_SED);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> entries.add(FRUIT_APP));
 	}
 }
