78559db7fc6ff28d4fb436a1fa42e1932bc7ae5d
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-12-08 10:16:08 +0800

    s

---
+++ remappedSrc/net/github/buiawpkgew1/item/ModFoodComponents.java
@@ -0,0 +1,10 @@
+package net.github.buiawpkgew1.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+  public static final FoodComponent FRUIT_APP = new FoodComponent.Builder().hunger(4).saturationModifier(0.1f).build();
+  public static final FoodComponent HABI_SED = new FoodComponent.Builder().hunger(4).saturationModifier(0.2f).build();
+  public static final FoodComponent HABI_LIN=new FoodComponent.Builder().hunger(4).saturationModifier(0.9f).build();
+
+}
