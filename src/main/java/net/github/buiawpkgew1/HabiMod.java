cffe5c6face02761d576f95d065d54920c2f7b11
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-07-20 11:29:58 +0800

    更新 HabiMod.java

--- src/main/java/net/github/buiawpkgew1/HabiMod.java
+++ src/main/java/net/github/buiawpkgew1/HabiMod.java
@@ -12,10 +12,47 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.lwjgl.glfw.GLFW;
 
 public class HabiMod implements ModInitializer {
     public static final Logger VERSION = LoggerFactory.getLogger("1.0");
 	public static final Logger LOGGER = LoggerFactory.getLogger("habi");
+// 夜视的快捷键
+    public static KeyBinding NIGHT_VISION_KEY_BINDING;
+
+    // 夜视的状态变量
+    public static boolean nightVisionEnabled = false;
+
+    @Override
+    public void onInitialize() {
+        // 创建快捷键绑定
+        NIGHT_VISION_KEY_BINDING = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding("key.night_vision", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F, "category.my_mod")
+        );
+
+        // 注册事件监听器
+
+
+
     // 新物品的实例
     public static Item FRUIT_APP = new Item(new Item.Settings().food(ModFoodComponents.FRUIT_APP));
     public static Item HABI_SED = new Item(new Item.Settings().food(ModFoodComponents.HABI_SED));
